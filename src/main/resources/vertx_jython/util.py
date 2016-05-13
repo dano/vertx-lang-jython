67e1125935e5fa2625e0a1762278ebdaf4d00b67
Dan O'Reilly <oreilldf@gmail.com>
2016-05-12 23:06:03 -0400

    Working on template.
    Signed-off-by: Dan O'Reilly <oreilldf@gmail.com>

--- src/main/resources/vertx_jython/util.py
+++ src/main/resources/vertx_jython/util.py
@@ -9,24 +9,17 @@ class VertxException(Exception):
 
 def make_handler(fut):
     def handler_wrapper(result):
-        print("result is {}".format(result))
         if result.succeeded():
             fut.set_result(result.result())
         else:
             fut.set_exception(result.cause())
     return handler_wrapper
 
-
-def parse_array(old_parse_array, *args, **kwargs):
-    values, end = old_parse_array(*args, **kwargs)
-    return frozenset(values), end
-
-
 def handle_none(obj, type_):
     return type_(obj) if obj is not None else obj
 
 def cached(func):
-    ''' Decorator for methods that need to cache results. '''
+    """ Decorator for methods that need to cache results. """
     dct = dict(cached_ret=None)
     @wraps(func)
     def inner(*args, **kwargs):
