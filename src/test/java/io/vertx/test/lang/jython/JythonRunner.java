a8e038efc9a077c7380b68c1e83c9c9e230bb5f7
Dan O'Reilly <oreilldf@gmail.com>
2016-11-26 01:21:39 -0500

    clean up
    Signed-off-by: Dan O'Reilly <oreilldf@gmail.com>

--- src/test/java/io/vertx/test/lang/jython/JythonRunner.java
+++ src/test/java/io/vertx/test/lang/jython/JythonRunner.java
@@ -40,44 +40,8 @@ public class JythonRunner {
       py.exec(moduleName + "." + testName + "()");
     }
   }
+
   private String getModuleName(String scriptName) {
     return scriptName.substring(0, scriptName.length() -3);
-  }
-
-  public int run(String scriptName, String testName, boolean provideRequire, 
-                 boolean provideConsole) throws Exception {
-    System.setProperty("python.options.internalTablesImpl","weak");
-    Options.includeJavaStackInExceptions = false;
-    PythonInterpreter py = new PythonInterpreter(null, new PySystemState());
-    try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(scriptName)) {
-      if (is == null) {
-        throw new IllegalArgumentException("Cannot find script: " + scriptName);
-      }
-      // We wrap the python script in a function so different instances don't see each others top level vars
-      String genName = "__VertxInternalVert__" + 1;
-      String funcName = "f" + genName;
-      StringBuilder sWrap = new StringBuilder();
-//      StringBuilder sWrap = new StringBuilder("def ").append(funcName).append("():\n");
-      BufferedReader br = new BufferedReader(new InputStreamReader(is));
-      for (String line = br.readLine(); line != null; line = br.readLine()) {
-        // Append line indented by a tab
-        sWrap.append(line).append("\n");
-      }
-      br.close();
-      sWrap.append("\n").append("TestAPI('").append(testName).append("')")
-      .append(".debug()");
-
-      // We have to convert it back to an inputstream since for some reason there is no version
-      // py.exec which takes a String AND a fileName - and without the filename
-      // any stack traces from errors won't show the filename and be hard for the user to parse.
-     System.out.println(sWrap.toString());
-      try (InputStream sis = new ByteArrayInputStream(sWrap.toString().getBytes("UTF-8"))) {
-        py.execfile(sis, scriptName);
-      }
-
-    } catch (Exception e) {
-      throw new VertxException(e);
-    }
-    return 0;
   }
 }
